3afe8bc6602253ee2c3ac225c2a0d7ccdb743d00
mperalta <mperalta@hexacta.com>
2018-09-18 17:34:26 -0300

    Se agre proyecto inicial con login

---
+++ Droid/Fragments/IFragmentVisible.cs
@@ -0,0 +1,7 @@
+﻿namespace HexactaXamarinNative.Droid
+{
+    interface IFragmentVisible
+    {
+        void BecameVisible();
+    }
+}
---
+++ HexactaXamarinNative/ViewModels/LoginViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using HexactaXamarinNative;
+
+namespace Frontend.Core.Areas.Login.ViewModel
+{
+    public class LoginViewModel  
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public ICommand LoginCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(DoLogin);
+        }
+
+        public void ValidateUsernameInput()
+        {
+        }
+
+        public void ValidatePasswordInput()
+        {
+        }
+
+        private void InitValidations()
+        {
+        }
+
+        private void DoLogin()
+        {
+           
+        }
+    }
+}
