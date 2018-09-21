c39b34dce4ee49a6df58fb64b4d09cbb49778394
mperalta <mperalta@hexacta.com>
2018-09-21 09:41:36 -0300

    Se agrega floating label y custom button

--- Droid/Activities/LoginActivity.cs
+++ Droid/Activities/LoginActivity.cs
@@ -1,24 +1,23 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+using Frontend.Core.Areas.Login.ViewModel;
 
 namespace HexactaXamarinNative.Droid.Activities
 {
     [Activity(Label = "LoginActivity")]
     public class LoginActivity : Activity
     {
+        private readonly LoginViewModel loginViewModel;
+
+        public LoginActivity()
+        {
+            this.loginViewModel = new LoginViewModel();
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_login);
 
             // Create your application here
         }
